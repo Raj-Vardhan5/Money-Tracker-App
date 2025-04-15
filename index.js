84df342aa5a501334c1261ae2d3ac38da44d0c11
Raj Vardhan Singh <104431808+Raj-Vardhan5@users.noreply.github.com>
2025-04-15 09:04:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-mongoose.connect('mongodb://localhost:27017/MoneyList')
+mongoose.connect('mongodb+srv://raj773313:0S538WXqJ3K3rRRW@cluster0.gtfacgl.mongodb.net/MoneyList')
 var db = mongoose.connection
 db.on('error', ()=> console.log("Error in connecting to the Database"))
 db.once('open', () => console.log("Connected to Database"))
